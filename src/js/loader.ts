49f4873528a32cdaa5b1a39381da9b3c4d8212dc
ArtemSafin-WebDev <artemsafin.webdev@gmail.com>
2025-12-09 08:38:35 +0300

    Анимация обводки в лоадере

--- src/js/loader.ts
+++ src/js/loader.ts
@@ -1,47 +1,56 @@
 import gsap from "gsap";
-import { SplitText } from "gsap/all";
+import {DrawSVGPlugin, SplitText} from "gsap/all";
 
-gsap.registerPlugin(SplitText);
+gsap.registerPlugin(SplitText, DrawSVGPlugin);
 
 export default function loader() {
-  const loader = document.querySelector<HTMLElement>(".loader");
-  if (!loader) return;
+    const loader = document.querySelector<HTMLElement>(".loader");
+    if (!loader) return;
 
-  const headingsAfterLoad = Array.from(
-    document.querySelectorAll<HTMLElement>(".js-animated-heading-after-load")
-  );
-
-  const splits = headingsAfterLoad.map((heading) =>
-    SplitText.create(heading, {
-      type: "words, chars",
-      linesClass: "line",
-      charsClass: "char",
-      wordsClass: "word",
-      mask: "words",
-    })
-  );
-
-  const tl = gsap.timeline();
-
-  tl.to(".loader", {
-    clipPath: "inset(0 0 100% 0)",
-    duration: 0.6,
-  })
-    .addLabel("loader-hidden")
-    .call(() => {
-      loader.remove();
-    });
-  if (document.fonts.status !== "loaded") return;
-  splits.forEach((split) => {
-    tl.from(
-      split.chars,
-      {
-        duration: 0.5,
-        yPercent: 100,
-        autoAlpha: 0,
-        stagger: 0.05,
-      },
-      "loader-hidden+=0.2"
+    const headingsAfterLoad = Array.from(
+        document.querySelectorAll<HTMLElement>(".js-animated-heading-after-load")
     );
-  });
+
+    const splits = headingsAfterLoad.map((heading) =>
+        SplitText.create(heading, {
+            type: "words, chars",
+            linesClass: "line",
+            charsClass: "char",
+            wordsClass: "word",
+            mask: "words",
+        })
+    );
+
+    const tl = gsap.timeline();
+
+
+    gsap.set(".loader .loader__pattern path", {
+        autoAlpha: 1
+    })
+    tl.from(".loader .loader__pattern path", {
+        duration: 1.5, drawSVG: 0,
+        ease: "power2.out"
+    })
+
+    tl.to(".loader", {
+        clipPath: "inset(0 0 100% 0)",
+        duration: 0.6,
+    })
+        .addLabel("loader-hidden")
+        .call(() => {
+            loader.remove();
+        });
+    if (document.fonts.status !== "loaded") return;
+    splits.forEach((split) => {
+        tl.from(
+            split.chars,
+            {
+                duration: 0.5,
+                yPercent: 100,
+                autoAlpha: 0,
+                stagger: 0.05,
+            },
+            "loader-hidden+=0.2"
+        );
+    });
 }
